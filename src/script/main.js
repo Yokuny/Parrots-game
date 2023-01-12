d2f5998356012c26fb0453c41fc92e027bfed0b3
Felipe Rangel Ribeiro <felipe.vni@hotmail.com>
2023-01-12 12:25:48 -0300

    card element creator

--- src/script/main.js
+++ src/script/main.js
@@ -1,36 +1,37 @@
 const parrot = (id) => {
   const getId = () => `id0${id}`;
-  let img;
+  let src;
   switch (id) {
     case 1:
-      img = "./src/img/bobrossparrot.gif";
+      src = "./src/img/bobrossparrot.gif";
       break;
     case 2:
-      img = "./src/img/fiestaparrot.gif";
+      src = "./src/img/explodyparrot.gif";
       break;
     case 3:
-      img = "./src/img/fiestaparrot.gif";
+      src = "./src/img/fiestaparrot.gif";
       break;
     case 4:
-      img = "./src/img/metalparrot.gif";
+      src = "./src/img/metalparrot.gif";
       break;
     case 5:
-      img = "./src/img/revertitparrot.gif";
+      src = "./src/img/revertitparrot.gif";
       break;
     case 6:
-      img = "./src/img/tripletsparrot.gif";
+      src = "./src/img/tripletsparrot.gif";
       break;
     case 7:
-      img = "./src/img/unicornparrot.gif";
+      src = "./src/img/unicornparrot.gif";
       break;
     default:
-      img = "./src/img/parrot.gif";
+      src = "./src/img/parrot.svg";
       break;
   }
-  return { getId, img };
+  return { getId, src };
 };
 
 function numberOfCards() {
+  //pegando a quantidade de cards
   let cardAmount = 0;
   while (cardAmount < 4 || cardAmount > 14 || cardAmount % 2 !== 0) {
     cardAmount = prompt("Digite a quantidade de cards (entre 4 a 14)");
@@ -39,12 +40,53 @@ function numberOfCards() {
 }
 
 function amountOfCards(amount) {
+  // criando os object com id e img src
   let cardList = [];
-  for (let i = 0; i <= amount; i++) {
+  for (let i = 1; i <= amount; i++) {
     const parrotCard = parrot(i);
     cardList.push(parrotCard);
   }
   return cardList;
 }
 
-let cards = amountOfCards(numberOfCards() / 2);
+function cardElements(obj) {
+  // criando os elemento html dos card
+  let allCards = [];
+  function theCard(id, imgSrc) {
+    const frontFaceImg = document.createElement("img");
+    frontFaceImg.alt = "Parrot";
+    frontFaceImg.src = "./src/img/parrot.svg";
+
+    const frontFace = document.createElement("div");
+    frontFace.classList.add("front-face");
+    frontFace.classList.add("face");
+    frontFace.appendChild(frontFaceImg);
+
+    const backFaceImg = document.createElement("img");
+    backFaceImg.src = imgSrc;
+
+    const backFace = document.createElement("div");
+    backFace.classList.add("back-face");
+    backFace.classList.add("face");
+    backFace.appendChild(backFaceImg);
+
+    const cardElement = document.createElement("div");
+    cardElement.classList.add("card");
+    cardElement.id = id;
+
+    cardElement.appendChild(frontFace);
+    cardElement.appendChild(backFace);
+
+    return cardElement;
+  }
+  obj.forEach((element) => {
+    allCards.push(theCard(element.getId(), element.src));
+  });
+  return allCards;
+}
+
+let allCard = cardElements(amountOfCards(numberOfCards() / 2));
+let field = document.getElementById("gameCanva");
+allCard.forEach((element) => {
+  field.appendChild(element);
+});
